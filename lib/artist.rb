adbe73cb345bb5602fc48df32577be8a1bb11fb5
github username <github email address>
2021-06-30 16:20:00 -0400

    commit tree 22b08fdf7741b2ad6ea9c272f20834e4b1200ed0

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,25 @@
+class Artist
+    attr_accessor :name
+    @@all = []
+    
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def new_song(title, genre)
+        Song.new(title, self, genre)
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def genres
+        songs.map {|song| song.genre}.uniq
+    end
+end
--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,26 @@
+class Doctor
+    attr_accessor :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def appointments
+        Appointment.all.select {|app| app.doctor == self}
+    end
+
+    def patients
+        appointments.map {|app| app.patient}.uniq
+    end
+end
